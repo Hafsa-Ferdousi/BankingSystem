02ffaa1218669f7397e8cb0195855ef56ef81fc1
Hafsa Ferdousi <hafsaferdousi7@gmail.com>
2025-11-14 18:48:40 +0600

    Update Data.java

--- Data.java
+++ Data.java
@@ -1,85 +1,94 @@
-
 package bankingapplication;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
+
+import java.io.*;
 import java.util.ArrayList;
+
 public class Data {
 
-  
     File file;
-    
-    public Data(){
 
-        file = new File("Files/Data");
-        if (!file.exists()){
-            try {//Exception Handling
+    public Data() {
+
+        // Create folder if not exists
+        File folder = new File("Files");
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+
+        // Create file with extension
+        file = new File("Files/Data.txt");
+
+        try {
+            if (!file.exists()) {
                 file.createNewFile();
-            } catch (Exception e){
-                e.printStackTrace();
-            };
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public ArrayList<Account> getAccounts(){
+    public ArrayList<Account> getAccounts() {
 
-        String text = "";
-
-        //Exception Handling
+        StringBuilder text = new StringBuilder();
 
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
             String s;
-            while ((s = br.readLine()) != null){
-                text = text + s;
+
+            while ((s = br.readLine()) != null) {
+                text.append(s).append("\n");
             }
+
             br.close();
-            
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }catch(IOException e){
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         ArrayList<Account> accounts = new ArrayList<>();
-        String array1[] = text.split("New Account");
 
-        //getting the account's information
-        
-        for (String v : array1){
-            if (!v.equals("")){
-            String[] array2 = v.split("A");
+        // split properly
+        String[] blocks = text.toString().split("NEW_ACCOUNT");
+
+        for (String block : blocks) {
+
+            block = block.trim();
+
+            if (block.isEmpty()) continue;
+
+            String[] arr = block.split("A");
+
             Account a = new Account();
-            a.setName(array2[0]);
-            a.setAccountNo(Integer.parseInt(array2[1]));
-            a.setBalance(Double.parseDouble(array2[2]));
-            a.setPassword(Integer.parseInt(array2[3]));
+            a.setName(arr[0]);
+            a.setAccountNo(Integer.parseInt(arr[1]));
+            a.setBalance(Double.parseDouble(arr[2]));
+            a.setPassword(Integer.parseInt(arr[3]));
+
             accounts.add(a);
-            }
         }
+
         return accounts;
     }
-        
-    public void saveAccounts(ArrayList<Account> accounts){
-        
-        String text = "";
-        for (Account a : accounts){
-            StringBuilder sb = new StringBuilder();
-            sb.append(a.getName()).append("A");
-            sb.append(String.valueOf(a.getAccountNo())).append("A");
-            sb.append(String.valueOf(a.getBalance())).append("A");
-            sb.append(String.valueOf(a.getPassword())).append("A");
-            text = text + sb.toString();
+
+    public void saveAccounts(ArrayList<Account> accounts) {
+
+        StringBuilder text = new StringBuilder();
+
+        for (Account a : accounts) {
+            text.append("NEW_ACCOUNT").append("\n");
+            text.append(a.getName()).append("A");
+            text.append(a.getAccountNo()).append("A");
+            text.append(a.getBalance()).append("A");
+            text.append(a.getPassword()).append("\n");
         }
+
         try {
             PrintWriter pw = new PrintWriter(file);
-            pw.print(text);
+            pw.print(text.toString());
             pw.close();
-        } catch ( FileNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+
